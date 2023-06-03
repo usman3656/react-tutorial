6ad5881cbcb6d3aa6084b7d4e36ee8e5c860a231
nawalirfan <nawal.irfan1110@gmail.com>
2023-06-04 03:37:56 +0500

    create order

--- src/Pages/CheckoutPage.jsx
+++ src/Pages/CheckoutPage.jsx
@@ -1,19 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react'
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
 import {BsCashCoin, BsCreditCardFill, BsRecordCircleFill} from 'react-icons/bs'
 import {AiFillHome, AiOutlineCheck, AiOutlinePlus} from 'react-icons/ai';
-import userData from '../utils/userData'
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function CheckoutPage() {
   const [step,setStep]=useState(1);  
-  const [address,setAddress]=useState(userData.address);
+  const [address,setAddress]=useState('');
   const [upAdd,setUpAdd]=useState(false);
   const [newAddress, setNewAddress] = useState('');
   const { cartItems,cartTotal,clearItemFromCart } = useContext(CartContext);
   const [orderno,setOrderno]=useState(12345);
+  const [Comments,setComments]=useState('');
+  const [type,setType]=useState('');
+
+  const id='643689de643a5e73aa5e56b2'//'64384b08e5912cec3662aa18' //will be changed after signin details stored
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    phone: '',
+    address: '',
+    city: '',
+    country: '',
+    role: ''
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await axios.get(`/user/getProfile/${id}`);
+        const userData = response.data.user;
+
+        setData({
+          firstName: userData.firstName,
+          lastName: userData.lastName,
+          username: userData.username,
+          phone: userData.phone,
+          address: userData.address,
+          city: userData.city,
+          country: userData.country,
+          role: userData.role
+        });        
+        setAddress(userData.address)
+      } catch (error) {
+        console.log('Error fetching profile:', error);
+      }
+    };
+
+    fetchProfile();
+  }, [id]);
 
   const updateAdd =() => {
     setUpAdd(true);
@@ -22,11 +61,58 @@ function CheckoutPage() {
     setAddress(address);
     setUpAdd(false);
   };
-  const handleStepClick = (newStep) => {
+  const handleStepClick = () => {
     setStep(step+1);
   };
   const handleUpdateAddress = () => {    
       updateAddress(newAddress);    
+  };
+
+  const handleCommentChange = (event) => {
+    setComments(event.target.value);
+  };
+
+  const handleOnlinePayment = ()=>{
+    setType('Online');
+  }
+
+  const handleCashPayment =()=>{
+    setType('Cash');
+  }
+
+  const createOrder = async () => {
+    try {
+      const orderData = {
+        Address: {
+          address: address,
+          city: data.city,
+          country: data.country,
+          phone: data.phone
+        },
+        orderItems: cartItems.map(item => ({
+          productID: item._id,
+          productName: item.productName,
+          productPrice: item.productPrice,
+          productQuantity: 1
+        })),
+        itemsPrice: cartTotal,
+        totalAmount: cartTotal + 300,
+        Comments: Comments,
+        user: id,
+        paymentType: type
+      };
+      console.log(orderData);
+  
+      const response = await axios.post('/user/create-order', orderData);
+      const createdOrder = response.data.order;
+  
+      setOrderno(createdOrder.orderNo); // Update the order number state variable
+  
+      // Proceed to the next step
+      setStep(step + 1);
+    } catch (error) {
+      console.log('Error creating order:', error);
+    }
   };
 
   return (
@@ -49,12 +135,12 @@ function CheckoutPage() {
             <div className='my-3 border border-warning rounded p-3'>
               <div>
                 <AiFillHome className='text-warning' size={25}/>
-                <span className='ms-2 fw-bold text-secondary'>{userData.firstName} {userData.lastName}</span>
+                <span className='ms-2 fw-bold text-secondary'>{data.firstName} {data.lastName}</span>
                 <div className='ms-4 ps-3 text-secondary'>
-                  {userData.phone}
+                  {data.phone}
                 </div>
                 <div className='ms-4 ps-3'>
-                  {address}
+                  {address}, {data.city}, {data.country}
                 </div>
               </div>
               
@@ -75,12 +161,16 @@ function CheckoutPage() {
           :
           step===2?
           <div> 
+              <div className='fs-5 offset-2 col-6 mb-3 fw-semibold'>
+                Any Comments for delivery? 
+                <input className='form-control' placeholder='comment?' value={Comments} onChange={handleCommentChange}></input>
+              </div>
             <div className='fs-5 offset-2 mb-3 fw-semibold'>
               Select payment method: 
             </div>
             <div className="form-check form-check-inline border rounded border-warning col-6 offset-2 my-2">
               <div className='m-2'>
-                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"/>
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" onClick={handleOnlinePayment}/>
                 <label className="form-check-label" for="inlineRadio1">
                   <div className='d-flex gap-2'>
                   <BsCreditCardFill className='text-warning' size={25}/>
@@ -91,7 +181,7 @@ function CheckoutPage() {
             </div>
             <div className="form-check form-check-inline border rounded border-warning col-6 offset-2 my-2">
               <div className='m-2'>
-                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"/>
+                <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" onClick={handleCashPayment}/>
                 <label className="form-check-label" for="inlineRadio2">
                   <div className='d-flex gap-2'>
                     <BsCashCoin className='text-warning' size={25}/>
@@ -102,7 +192,7 @@ function CheckoutPage() {
             </div>
             <div className='my-3 d-flex justify-content-between col-6 offset-2'>
               <div>Order total:  <span className='fw-bold'> Rs. {cartTotal}</span></div>
-              <button className='btn btn-warning' onClick={handleStepClick}> Place Order</button>
+              <button className='btn btn-warning' onClick={createOrder}> Place Order</button>
             </div>
           </div>
           :
